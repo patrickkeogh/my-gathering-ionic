8b19e907de27d9b418bc9413a0985623ac1a3b8f
patrickkeogh <patrickkeogh@kantechprogramming.com>
2017-04-24 09:08:17 -0400

    Add Infinite scrolling to search page

--- www/js/controllers/search.ctr.js
+++ www/js/controllers/search.ctr.js
@@ -5,11 +5,13 @@
     .module('myGathering')
     .controller('SearchController', SearchController);
 
-  SearchController.$inject = ['$scope', '$rootScope', '$state', 'gatheringAPI', 'Utils', '$ionicModal', '$ionicPlatform', '$ionicLoading'];
+  SearchController.$inject = ['$scope', '$rootScope', '$state', '$timeout', 'gatheringAPI', 'Utils', '$ionicModal', '$ionicPlatform', '$ionicLoading'];
 
-  function SearchController($scope, $rootScope, $state, gatheringAPI, Utils, $ionicModal, $ionicPlatform, $ionicLoading) {
+  function SearchController($scope, $rootScope, $state, $timeout, gatheringAPI, Utils, $ionicModal, $ionicPlatform, $ionicLoading) {
 
     $scope.gatherings = {};
+
+    $scope.numToShow = 0;
  
     //$scope.gathering.name = "Gathering Details";
 
@@ -28,6 +30,8 @@
     });
 
 
+
+
     function getGatherings(query) {
       //console.log("Query Used:" + JSON.stringify(query));
 
@@ -40,6 +44,39 @@
         console.log('failed to get gatherings: ' + err);
       });
     }
+
+    $scope.addMoreItem = function(done) {
+        if ($scope.gatherings.length > $scope.numToShow) {
+
+         $scope.numToShow += 5; // load 20 more items
+        done(); // need to call this when finish loading more data
+      }
+    };
+
+
+    $scope.getRecs = function() {
+      console.log("GetRecs call");
+
+      //Add 5 more recs to the list
+
+      $timeout(function(){
+        // some function i wrote
+        $scope.numToShow += 5;
+        $scope.$broadcast('scroll.infiniteScrollComplete');
+        $scope.$broadcast('scroll.refreshComplete');
+
+     }, 5000);
+
+      
+
+
+
+      
+    };
+
+    $scope.areThereMoreRecsToShow = function() {
+      return ($scope.gatherings.length > $scope.numToShow) ? true : false;
+    };
 
     $scope.goToDetails = function(gathering) {
       console.log('goToDetails:' + gathering._id);
